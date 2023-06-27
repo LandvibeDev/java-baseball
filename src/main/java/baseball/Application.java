a478123f46c203ac355f78db7b7be0797b9bfaa9
박준영 <wnsdud6969@naver.com>
2023-06-27 22:20:07 +0900

    feat(Application): add function to repeat game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -124,6 +124,15 @@ public class Application {
 		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 	}
 
+	public static boolean repeatGame() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		String isRepeat = Console.readLine();
+		if (isRepeat.equals("2")) {
+			return false;
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
 		//TODO: 숫자 야구 게임 구현
 	}
