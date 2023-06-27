3d9dc902fd549ae1f1af426923f07dccf923219d
박준영 <wnsdud6969@naver.com>
2023-06-27 20:14:19 +0900

    feat(Application): add function to generate random number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-    }
+
+	public static String generateRandomNumber() {
+		StringBuilder randomNumber = new StringBuilder();
+
+		while (randomNumber.length() < 3) {
+			String randomDigit = String.valueOf(Randoms.pickNumberInRange(1, 9));
+
+			if (randomNumber.indexOf(randomDigit) == -1) {
+				randomNumber.append(randomDigit);
+			}
+		}
+
+		return randomNumber.toString();
+	}
+
+	public static void main(String[] args) {
+		//TODO: 숫자 야구 게임 구현
+	}
 }
