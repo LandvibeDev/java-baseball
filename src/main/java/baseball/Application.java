d42505bbf10a70d33a6fd2a006ba6be759db2b5f
gimdongha <d11210920@gmail.com>
2023-06-28 19:25:07 +0900

    feat(Application):add function to input number that user guess

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,11 +2,17 @@ package baseball;
 
 import baseball.ball.Ball;
 import baseball.ball.BallBoundary;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static String createRandomBall(){
         Ball ball = new Ball(BallBoundary.MAX_VALUE.getValue(), BallBoundary.MIN_VALUE.getValue());
         return ball.getBall_number();
+    }
+    public static String input(){
+        System.out.print("숫자를 입력해주세요 : ");
+        String temp = Console.readLine();
+        return temp;
     }
 
     public static void main(String[] args) {
