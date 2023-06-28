756584927cf8e08e746e502ac89e7f83f543453e
HyeongSeok <gudtjr051911@gmail.com>
2023-06-29 00:46:37 +0900

    feat(BaseballGame): game result checking methods
    two methods
    - checkBall()
    - checkStrike()

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -20,4 +20,27 @@ public class BaseballGame {
             inputNumbers[i] = input.charAt(i) - '0';
         }
     }
+
+    private int checkStrike() {
+        int strike = 0;
+        for (int i=0; i<3; i++) {
+            if (randomNumbers[i] == inputNumbers[i]) {
+                strike++;
+                inputNumbers[i] = -1;
+            }
+        }
+        return strike;
+    }
+
+    private int checkBall() {
+        int ball = 0;
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3; j++) {
+                if (randomNumbers[i] == inputNumbers[j]) {
+                    ball++;
+                }
+            }
+        }
+        return ball;
+    }
 }
