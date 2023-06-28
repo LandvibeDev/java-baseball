b57fe16ee8923b8453b34ff59f96980767ab7cc9
gimdongha <d11210920@gmail.com>
2023-06-28 19:39:22 +0900

    feat(Application):add function to check legal format of created number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -15,6 +15,21 @@ public class Application {
         return temp;
     }
 
+    public static void checkException(String number){
+        if(number.length() != 3) {
+            throw new IllegalArgumentException(); // 세자리 수
+        }
+        if(number.contains("0")) {
+            throw new IllegalArgumentException(); // 0이 있는지
+        }
+        for(int i = 0; i < number.length(); i++){
+            if(!(number.charAt(i) > 0 && number.charAt(i) <= 9)) throw new IllegalArgumentException(); //숫자가 아닌지
+        }
+        if(number.length() != 3)throw new IllegalArgumentException(); // 길이가 다른지
+
+    }
+
+
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
     }
