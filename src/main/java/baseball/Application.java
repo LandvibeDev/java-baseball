05560b98e0f4ca7b3c4387a8570d6c472f5f8092
gimdongha <d11210920@gmail.com>
2023-06-28 19:23:24 +0900

    feat(Application):add function to generate ball object and ball number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,14 @@
 package baseball;
 
+import baseball.ball.Ball;
+import baseball.ball.BallBoundary;
+
 public class Application {
+    public static String createRandomBall(){
+        Ball ball = new Ball(BallBoundary.MAX_VALUE.getValue(), BallBoundary.MIN_VALUE.getValue());
+        return ball.getBall_number();
+    }
+
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
     }
