e5714771aced1486fc0811557a7517fa7c65a689
genius00hwan <qnfn120@gmail.com>
2023-06-29 18:24:07 +0900

    protorype

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import java.util.Scanner;
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-    }
+	static int targetNum = 123;
+	static int targetFirst = targetNum / 100;
+	static int targetSecond = (targetNum % 100) / 10;
+	static int targetThird = targetNum % 10;
+
+	public static void main(String[] args) {
+		//TODO: 숫자 야구 게임 구현
+		Application app = new Application();
+		int in = app.input();
+
+	}
+
+	public int input() {
+		int in = 0;
+		Scanner scnr = new Scanner(System.in);
+		System.out.print("숫자를 입력해주세요");
+		scnr.nextInt(in);
+		return in;
+	}
 }
+
+
+
---
+++ src/main/java/baseball/Scoring.java
@@ -0,0 +1,52 @@
+package baseball;
+
+import static baseball.Application.*;
+
+public class Scoring {
+	public class scoreDTO {
+		int strike;
+		int ball;
+		boolean correct;
+	}
+	public class judge {
+		public scoreDTO scoring(int in) {
+			int first = in / 100;
+			int second = (in % 100) / 10;
+			int third = in % 10;
+			scoreDTO score = new scoreDTO();
+			score.strike = countStrike(first, second, third);
+			score.ball = countBall(first, second, third);
+			score.correct = (score.strike == 3);
+
+			return score;
+		}
+
+		public int countStrike(int first, int second, int third) {
+			int strike = 0;
+			if (first == targetFirst)
+				strike++;
+			if (second == targetSecond)
+				strike++;
+			if (third == targetThird)
+				strike++;
+			return strike;
+		}
+
+		public int countBall(int first, int second, int third) {
+			int ball = 0;
+			if (first != targetFirst) {
+				if (first == targetSecond || first == targetThird)
+					ball++;
+			}
+			if (second != targetSecond) {
+				if (second == targetFirst || second == targetThird)
+					ball++;
+			}
+			if (third != targetThird) {
+				if (third == targetFirst || third == targetSecond)
+					ball++;
+			}
+			return ball;
+		}
+	}
+}
