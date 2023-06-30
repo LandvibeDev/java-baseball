717d9588e3834740e1404ce5c330e0fd74064584
임주민 <limj1213@gmail.com>
2023-06-30 19:27:02 +0900

    feat(Constants): add a constant file with number and message

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,18 @@
+package baseball;
+
+public final class Constants {
+    public final class Number {
+        public static final int LENGTH = 3;
+        public static final int MIN_VALUE = 0;
+        public static final int MAX_VALUE = 9;
+    }
+
+    public final class Message {
+         public static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+         public static final String END_GAME = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+         public static final String RE_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+        public static final String STRIKES = "스트라이크";
+        public static final String BALLS = "볼 ";
+        public static final String NOTHING_ = "낫싱";
+    }
+}
