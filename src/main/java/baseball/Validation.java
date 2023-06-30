38e1fff6133c7a47878a4a452a301defc6ff5054
HyeongSeok <gudtjr051911@gmail.com>
2023-06-30 15:19:11 +0900

    refactor(Validation): seperate methods

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -6,34 +6,40 @@ import java.util.regex.Pattern;
 
 public class Validation {
 
-    String inputString;
-
-    Validation(String inputString) {
-        this.inputString = inputString;
+    public void validateInput(String inputString) {
+        lengthCheck(inputString);
+        characterCheck(inputString);
+        duplicationCheck(inputString);
     }
 
-    public void handleException() {
-        lengthCheck();
-        wrongInputCheck();
-    }
-
-    private void lengthCheck() {
-        if (inputString.length() != BaseballGame.number) {
-            throw new IllegalArgumentException("정해진 개수의 숫자만 입력해주세요");
+    private void lengthCheck(String inputString) {
+        if (inputString.length() != Rule.number) {
+            throw new IllegalArgumentException(ExceptionMessage.WRONG_LENGTH.message);
         }
     }
 
-    private void wrongInputCheck() {
-        Set<Integer> set = new HashSet<>();
-        String pattern = "^[" + BaseballGame.start + "-" + BaseballGame.end +"]*$";
-        for (int i = 0; i < BaseballGame.number; i++) {
+    private void characterCheck(String inputString) {
+        String pattern = makeRegularExpression();
+        for (int i = 0; i < Rule.number; i++) {
             if (!Pattern.matches(pattern, inputString)) {
-                throw new IllegalArgumentException("정해진 범위 안의 숫자만 입력해주세요");
+                throw new IllegalArgumentException(ExceptionMessage.WRONG_CHARACTER.message);
             }
+        }
+    }
+
+    private String makeRegularExpression() {
+        return "^[" + Rule.start + "-" + Rule.end +"]*$";
+    }
+
+    private void duplicationCheck(String inputString) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < inputString.length(); i++) {
             set.add(inputString.charAt(i) - '0');
         }
-        if (set.size() != BaseballGame.number) {
-            throw  new IllegalArgumentException();
+        if (set.size() != inputString.length()) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATION.message);
         }
     }
+
+
 }
