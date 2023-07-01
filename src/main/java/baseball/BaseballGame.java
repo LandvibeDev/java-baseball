a59afb6d424ed76e5128ae373edf8b01fe0e6a7f
sheepysnoopy <qkffldlfdjsk12@naver.com>
2023-07-01 17:45:36 +0900

    feat(Message) add Enum

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -92,10 +92,6 @@ public class BaseballGame {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String cmd = Console.readLine();
 
-        if(!cmd.equals(1) && !cmd.equals(2)){
-            throw new IllegalArgumentException();
-        }
-
         if(cmd.equals("1")){
             return true;
         }
--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -1,2 +1,15 @@
-package baseball;public enum Message {
+package baseball;
+
+public enum Message {
+    INPUT_USERNUMBER_MESSAGE("숫자를 입력해 주세요 : "),
+    STRIKE_MESSAGE("스트라이크"),
+    BALL_MESSAGE("볼 "),
+    NOTHING_MESSAGE("낫싱"),
+    RESTART_QUESTION_MESSAGE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    ;
+
+    private String message;
+    Message(String message) {
+        this.message = message;
+    }
 }
