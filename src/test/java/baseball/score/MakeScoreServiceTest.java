1216cc88da82ee751dfb90f310d1db5951c2d295
leesc0839 <leesc0839@naver.com>
2023-07-02 22:24:57 +0900

    feat(MakeScoreServiceTest)

--- src/test/java/baseball/score/MakeScoreServiceTest.java
+++ src/test/java/baseball/score/MakeScoreServiceTest.java
@@ -1,17 +1,49 @@
 package baseball.score;
 
+import baseball.AppConfig;
+import baseball.number.MakeInputNumberService;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class MakeScoreServiceTest {
 
+    AppConfig testAppConfig = new AppConfig();
+    MakeScoreService makeScoreService = testAppConfig.makeScoreService();
+    MakeInputNumberService makeInputNumberService = testAppConfig.makeInputNumberService();
+
     @Test
-    void compareNumbersAndMakeScore() {
+    void score_결과_테스트() {
         //given
+        Map<Integer, Integer> randomNumberMap135 = makeInputNumberService.makeInputNumberMap("135");
+        Map<Integer, Integer> randomNumberMap589 = makeInputNumberService.makeInputNumberMap("589");
+
+        Map<Integer, Integer> inputNumberMap246 = makeInputNumberService.makeInputNumberMap("246");
+        Map<Integer, Integer> inputNumberMap135 = makeInputNumberService.makeInputNumberMap("135");
+        Map<Integer, Integer> inputNumberMap597 = makeInputNumberService.makeInputNumberMap("597");
 
         //when
+        Score score_135_246 = makeScoreService.compareNumbersAndMakeScore(randomNumberMap135, inputNumberMap246);
+        Score score_135_135 = makeScoreService.compareNumbersAndMakeScore(randomNumberMap135, inputNumberMap135);
+        Score score_589_246 = makeScoreService.compareNumbersAndMakeScore(randomNumberMap589, inputNumberMap597);
+        Score score_589_589 = makeScoreService.compareNumbersAndMakeScore(randomNumberMap589, inputNumberMap246);
+
 
         //then
+        assertThat(score_135_246.getStrike()).isEqualTo(0);
+        assertThat(score_135_246.getBall()).isEqualTo(0);
+
+        assertThat(score_135_135.getStrike()).isEqualTo(3);
+        assertThat(score_135_135.getBall()).isEqualTo(0);
+
+        assertThat(score_589_246.getStrike()).isEqualTo(0);
+        assertThat(score_589_246.getBall()).isEqualTo(0);
+
+        assertThat(score_589_589.getStrike()).isEqualTo(3);
+        assertThat(score_589_589.getBall()).isEqualTo(0);
+
     }
 }
