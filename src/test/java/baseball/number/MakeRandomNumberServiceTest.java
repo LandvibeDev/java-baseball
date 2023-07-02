396223fbd16f814f3bcb368897346ee80d216960
leesc0839 <leesc0839@naver.com>
2023-07-02 22:04:25 +0900

    feat(MakeRandomNumberServiceTest)

--- src/test/java/baseball/number/MakeRandomNumberServiceTest.java
+++ src/test/java/baseball/number/MakeRandomNumberServiceTest.java
@@ -2,32 +2,27 @@ package baseball.number;
 
 import baseball.AppConfig;
 import baseball.constant.Number;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 class MakeRandomNumberServiceTest {
-
-    @Test
-    void makeRandomNumberMap() {
-    }
 
     AppConfig testAppConfig = new AppConfig();
     MakeRandomNumberService makeRandomAnswerService = testAppConfig.makeRandomNumberService();
 
     @Test
-    void 랜덤_숫자_생성확인() {
+    void 랜덤_숫자_생성_성공() {
         //given
 
         //when
         Map<Integer, Integer> randomNumberMap = makeRandomAnswerService.makeRandomNumberMap();
 
         //then
-        Assertions.assertFalse(randomNumberMap.containsKey(0));
-        Assertions.assertEquals(Number.LENGTH_OF_INPUT.value(), randomNumberMap.size());
-
+        assertThat(Number.LENGTH_OF_INPUT.value()).isEqualTo(randomNumberMap.size());
+        assertThat(randomNumberMap.containsKey(0)).isEqualTo(false);
     }
 }
